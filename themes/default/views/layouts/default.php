90e6ddc11d244bc7f2d025dfe7b9b0ed4d3fbdd0
ebyjane <ebyjane@yahoo-inc.com>
2014-05-03 18:17:03 +0530

    most commented and most liked
    most commented and most liked

--- themes/default/views/layouts/default.php
+++ themes/default/views/layouts/default.php
@@ -31,21 +31,56 @@ $this->beginContent('//layouts/main'); ?>
                     <div class="box">
                     	<h2 class="title">Most Commented</h2>
                         <ul class="group">
-                        	<li>Problem with Samsung touch screen with Samsung touch screen</li>
-                            <li>How is Iron man 3 movie</li>
+						<?php
+							$sql = "SELECT content_id, COUNT( * ) AS `num` FROM comments GROUP BY content_id LIMIT 0 , 3";
+							$command = Yii::app()->db->createCommand($sql);
+							$results1 = $command->queryAll();
+						?>
+						
+						<?php foreach ($results1 as $key=>$value): ?>
+						
+						<?php 
+							$idVal = $value['content_id'];
+							$sql = "SELECT title FROM content WHERE id = '".$idVal."'";
+							$command = Yii::app()->db->createCommand($sql);
+							$results = $command->queryAll();						
+							echo "<li>".$results[0]['title']."</li>";
+							//echo "<br/>";
+						?>
+						
+						<?php endforeach; ?>
+                        	<!--<li>Problem with Samsung touch screen with Samsung touch screen</li>
+                            <li>How is Iron man 3 movie</li>-->
                             <li><a href="#" class="readmore right">Read more &raquo;</a></li>
                         </ul>
                     </div>
+					
+
                     
                     <div class="box">
                     	<h2 class="title most-like">Most Liked Comment</h2>
                         <ul class="group">
-                        	<li>Problem with Samsung touch screen with Samsung touch screen</li>
-                            <li>How is Iron man 3 movie</li>
+						<?php
+						 
+							$sql = "SELECT comment FROM `comments` WHERE like_count >0 ORDER BY like_count DESC LIMIT 0 , 3";
+							$command = Yii::app()->db->createCommand($sql);
+							$results1 = $command->queryAll();
+						?>
+						
+						<?php foreach ($results1 as $key=>$value): ?>
+						
+						<?php 
+							echo "<li>".$value['comment']."</li>";
+							//echo "<br/>";
+						?>
+						
+						<?php endforeach; ?>
                             <li><a href="#" class="readmore right">Read more &raquo;</a></li>
                         </ul>
                     </div>
-                    
+                    <?php 
+					if(!Yii::app()->user->isGuest){
+					?>
                     <div class="box">
                     	<h2 class="title expert">Questions for Experts</h2>
                         <ul class="group">
@@ -55,7 +90,7 @@ $this->beginContent('//layouts/main'); ?>
                             <li><a href="#" class="readmore right">Read more &raquo;</a></li>
                         </ul>
                     </div>
-                    
+                    <?php } ?>
                     <div class="box advertisement">
                     	<h3>Advertisements</h3>
                         <img src="images/add1.jpg" />
